03a0a291b69358b4fc422c692da2d8753a35328e
tlishchynskiy <4enutl@gmail.com>
2024-02-07 15:25:13 +0200

    Add: API simple versioning

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes - Versioned (V.1)
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
